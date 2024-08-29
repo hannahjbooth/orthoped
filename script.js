46eca14a3f03d93c04f2b7bc0864faa4918c2602
Hannah Booth <hannahjbooth9@gmail.com>
2024-08-29 19:31:43 +0100

    tested site search method

---
+++ script.js
@@ -0,0 +1,16 @@
+// Search button in navigation bar
+
+const searchForm = document.getElementById('search');
+const query = document.getElementById('query');
+const googleSearch = 'https://www.google.com/search?q=site%3A+';
+const site = 'orthoped.co.uk';
+
+function produceGoogleSearch(event) {
+    event.preventDefault();
+    const url = googleSearch + site + '+' + query.value;
+    const newWindow = window.open(url, '_blank');
+    window.focus();
+}
+
+searchForm.addEventListener('submit', produceGoogleSearch);
+
