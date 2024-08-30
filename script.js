324e2cdf6a65f4cfb2806984236617f94420598c
Hannah Booth <hannahjbooth9@gmail.com>
2024-08-30 09:34:55 +0100

    testing search feature method

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-// Search button in navigation bar
+// // Search button in navigation bar
 
-const searchForm = document.getElementById('search');
-const query = document.getElementById('query');
-const googleSearch = 'https://www.google.com/search?q=site%3A+';
-const site = 'orthoped.co.uk';
+// const searchForm = document.getElementById('search');
+// const query = document.getElementById('query');
+// const googleSearch = 'https://www.google.com/search?q=site%3A+';
+// const site = 'orthoped.co.uk';
 
-function produceGoogleSearch(event) {
-    event.preventDefault();
-    const url = googleSearch + site + '+' + query.value;
-    const newWindow = window.open(url, '_blank');
-    window.focus();
-}
+// function produceGoogleSearch(event) {
+//     event.preventDefault();
+//     const url = googleSearch + site + '+' + query.value;
+//     const newWindow = window.open(url, '_blank');
+//     window.focus();
+// }
 
-searchForm.addEventListener('submit', produceGoogleSearch);
+// searchForm.addEventListener('submit', produceGoogleSearch);
+
+// Let search queries open a new page containing search results
+
+
 
