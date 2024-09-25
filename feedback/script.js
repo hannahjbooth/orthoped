cfbeff0858f3ae416df0e7e390f3d50755f1ae5d
Hannah Booth <hannahjbooth9@gmail.com>
2024-09-25 15:54:54 +0100

    removed excess comments

--- feedback/script.js
+++ feedback/script.js
@@ -1,25 +1,13 @@
-// Handling the feedback form required checkboxes
+// Handling 'required' feature on checkbox for discomfort question
 
-    // Pseudocode
-    /*
-    IF user selects one checkbox (of which name="discomfort")
-        FOR all input elements of type "checkbox"
-            IF input element has an attribute of required
-                REMOVE all checkbox input elements' required attribute
-    */
-
-    // Select all checkbox inputs from the discomfort question
     let discomfortCheckboxes = document.getElementsByName("discomfort");
 
-
-    // Function that removes required attribute from checkboxes
     function removeRequiredFromCheckboxes(checkboxes) {
         checkboxes.forEach(checkbox => {
             checkbox.removeAttribute('required');
         });
     }
 
-    // Event listener added to all checkboxes to trigger function removing required attribute
     discomfortCheckboxes.forEach(checkbox => {
         checkbox.addEventListener('change', function() {
             removeRequiredFromCheckboxes(discomfortCheckboxes);
@@ -28,19 +16,9 @@
 
 // Handling the 'Other' checkbox's text input, so that it becomes required when the checkbox is selected
 
-    // Pseudocode
-    /*
-    IF user selects 'Other' checkbox
-        ADD required attribute to text input element
-    IF user deselects 'Other' checkbox
-        REMOVE required attribute to text input element
-    */
-
-    // Variables
     let otherCheckbox = document.getElementById("discomfort-other");
     let otherTextInput = document.getElementById("discomfort-other-text")
 
-    // Function to handle required attribute
     function handleRequiredOnTextInput(checkbox) {
         if (checkbox.checked) {
             otherTextInput.setAttribute("required", "");
@@ -49,7 +27,6 @@
         }
     }
 
-    // Add an event listener to the otherCheckbx
     otherCheckbox.addEventListener("change", function() {
         handleRequiredOnTextInput(otherCheckbox);
     });
