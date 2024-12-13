3eebf53a776ac912fda095f27be820a64ab6117c
Hannah Booth <hannahjbooth9@gmail.com>
2024-12-13 11:56:28 +0000

    updated search bar event listener and functions

--- script.js
+++ script.js
@@ -1,44 +1,23 @@
-// Search button in navigation bar
+// Search bar
 
-// const searchForm = document.getElementById('search');
-// const query = document.getElementById('query');
-// const googleSearch = 'https://www.google.com/search?q=site%3A+';
-// const site = 'orthoped.co.uk';
+const searchLink = document.getElementById("search-link");
+const searchBox = document.getElementById("search-box");
+const overlay = document.getElementById("overlay");
 
-// function produceGoogleSearch(event) {
-//     event.preventDefault();
-//     const url = googleSearch + site + '+' + query.value;
-//     const newWindow = window.open(url, '_blank');
-//     window.focus();
-// }
+searchLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    toggleSearchBox();
+    toggleDarkeningEffect();
+});
 
-// searchForm.addEventListener('submit', produceGoogleSearch);
+function toggleSearchBox() {
+    searchBox.classList.toggle("hidden");
+}
 
+function toggleDarkeningEffect() {
+    overlay.classList.toggle("darkening-effect");
+}
 
-
-
-// --- HEADER
-
-    // --- SEARCH
-
-    // IF "Search" is clicked
-        // LET Search input area appear below the main navigation bar
-        // LET all of main and footer content darken
-
-    const searchLink = document.getElementById("search-link");
-    const searchBox = document.getElementById("search-box");
-    const areasToOverlay = document.getElementsByClassName("overlay");
-
-    searchLink.addEventListener("click", function(event) {
-        // prevent event default
-        event.preventDefault();
-        // Toggle appearance of search box when "Search" is clicked
-        searchBox.classList.toggle("hidden");
-        // Darken Main area of the page when "Search" is clicked
-        for (const area of areasToOverlay) {
-            area.classList.toggle("darkening-effect");
-        }
-    });
 
     // If text is in the search input bar, keep new padding-bottom of 10px
     const searchInput = document.getElementById("query");
