69af68ab4227047db91e9b38ae86f414c619537a
Hannah Booth <hannahjbooth9@gmail.com>
2024-12-18 18:00:22 +0000

    added accordeon script to main script file

--- script.js
+++ script.js
@@ -106,6 +106,20 @@ function addActiveClassTo(currentSection) {
     }
 }
 
+// Accordeon
+
+let questions = Array.from(document.getElementsByClassName('accordeon-button'));
+
+for (let question of questions) {
+    question.addEventListener('click', () => {
+        let parent = question.parentElement;
+        let answer = parent.querySelector('.accordeon-content');
+
+        answer.classList.toggle('visible');
+    })
+}
+
+
 
 
 
