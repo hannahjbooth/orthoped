8a35304f0d69ac38deebc691b6959e17927639f3
Hannah Booth <hannahjbooth9@gmail.com>
2025-01-28 15:58:44 +0000

    added js file to test shop file

---
+++ shop/test/script.js
@@ -0,0 +1,20 @@
+/* <table cellspacing="0" cellpadding="0" class="ecwid-productBrowser-innerTable" style="width: 100%;"><tbody><tr><td align="left" style="vertical-align: top;"><table class="ecwid-productBrowser-auth-mini ecwid-productBrowser-auth-mini-horizontal" style=""><colgroup><col></colgroup><tbody><tr style="vertical-align: middle;"><td width="100%"><div class="ecwid-productBrowser-auth"><div></div></div></td></tr></tbody></table></td></tr><tr><td align="left" style="vertical-align: top;"><div style="display: none;" aria-hidden="true"><div class="gwt-HTML"></div></div></td></tr><tr><td align="left" style="vertical-align: top;"><div aria-hidden="true" style="display: none;"></div></td></tr></tbody></table> */
+
+/* <table class="ecwid-productBrowser-auth-mini ecwid-productBrowser-auth-mini-horizontal" style=""><colgroup><col></colgroup><tbody><tr style="vertical-align: middle;"><td width="100%"><div class="ecwid-productBrowser-auth"><div></div></div></td></tr></tbody></table> */
+
+
+// Pseudocode
+
+/*
+
+LET array allTables
+ITERATE through elements of <body>
+    IF element is <table>
+        ADD to allTables
+ITERATE through allTables
+    FOR each <table> of allTables
+        LOG <table>
+    IF allTables[index] > 2
+        FIND a way to target the two above <table> elements
+
+*/
